218a3346294dbdca5eda64ff89901cf01d14a08b
pl.hancharou@gmail.com <pl.hancharou@gmail.com>
2020-08-19 15:59:42 +0300

    fixed: toggle for bookmark & recomended block

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   var tab = $('.tab');
+  var save = $('.save');
 
   tab.on('click', function () {
     tab.removeClass('tabs-recomended__card_active');
@@ -7,6 +8,10 @@ $(document).ready(function () {
     var activeTabContent = $(this).attr('data-target');
     $('.tabs-content').removeClass('tabs-content__visible');
     $(activeTabContent).toggleClass('tabs-content__visible');
+  });
+
+  save.on('click', function () {
+    $(this).toggleClass('newsfeed-card__content_save');
   })
 });
 
