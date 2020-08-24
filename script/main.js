3cfb14b1f4e00bf2b949a55424768a0b9cc12a37
pl.hancharou@gmail.com <pl.hancharou@gmail.com>
2020-08-24 21:14:21 +0300

    create: responsive for header & ads sections

--- script/main.js
+++ script/main.js
@@ -23,7 +23,59 @@ $(document).ready(function () {
     $(hiddenComments).toggleClass('comments-item__list_hidden');
   });
 
+  //header menu
+  var menuSections = $('.button-menu');
+
+  menuSections.on('click', function () {
+    $('.header-bottom').toggleClass('header-bottom__active');
+    $('.header-top__search').toggleClass('header-top__search_active');
+    $('.header-top__info').toggleClass('header-top__info_active');
+    $('.header-top__logo').toggleClass('header-top__logo_active');
+    $('.secondary-director').toggleClass('secondary-director_active');
+  });
+
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+    /*Если сделали скролл на 100px задаём новый класс для header*/
+    if (height > 250) {
+      $('.header-bottom').addClass('header-bottom__fixed');
+    } else {
+      /*Если меньше 100px удаляем класс для header*/
+      $('.header-bottom').removeClass('header-bottom__fixed');
+    }
+  });
+
   //initialize swiper when document ready
+  var adsList = new Swiper('.ads-list', {
+    // Optional parameters
+    loop: true,
+    speed: 400,
+    slidesPerView: 4,
+    spaceBetween: 0,
+    breakpoints: {
+      993: {
+        slidesPerView: 4,
+        spaceBetween: 0
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 0
+      },
+      577: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      426: {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+  });
+
   var informationSlider = new Swiper('.information-block__slider', {
     // Optional parameters
     loop: true,
